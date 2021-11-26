d2dda2a7ca578f4805bea508ab1191a366a712d6
mbrown6 <matt.brown6@wework.com>
2021-11-26 12:11:31 -0800

    Adding sh_reth13.sh to GitHub

---
+++ Bash/juniper/sh_reth13.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Author: M Brown - matt.brown6@wework.com
+# Date: 25 November 2021
+
+# Simple Bash script to extract reth1/3.0 IP addresses from Juniper SRX core firewalls
+# Uses textfile test1.txt which is a list of host IP addresses
+
+# Load host IPs into devices var
+devices=$(cat test1.txt)
+
+# Prompt for user name, -p switch prints prompt text
+read -p  "Enter user name: " userName
+
+# Prompt for password, -s switch does not echo input tp stdout stream
+read -p "Enter password: " -s passWord
+
+# Juniper operational command plus regex to filter output
+commands="show interfaces terse | match ^reth[13]\.0"
+
+# Loop through devices var, use sshpass to store secret.
+# SSH -o(options) No fingerprinting, timeout 4sec (optimize for unresponsive targets)
+for i in ${devices}; do
+
+    echo "Connecting to device: ${i}"
+    sshpass -p$passWord ssh -o StrictHostKeyChecking=no -o ConnectTimeout=4 $userName@${i} ${commands}
+    echo "============================================================="
+
+done
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # net-scripts
 Network scripts
+- git clone https://github.com/mbrown6/net-scripts.git 
+- cd net-scripts
+- git checkout
+- Then after working:
+- git add -A
+- git commit -a -m "Testing commit"
+- git add -A
+- git commit -a -m "Another commit, changed some things"
+- git push
+- git pull
